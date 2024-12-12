9ce54d23c9e337e76a17df95fc3d243c81a6c2cc
lethanhtung22032005 <lethanhtungdeptrai@gmail.com>
2024-12-12 12:54:17 +0700

    Add files via upload

---
+++ Session14 ex1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>  
+
+using namespace std;
+
+int main() {
+    string input; i
+    cout << "Nhap vao mot chuoi: ";
+    getline(cin, input); 
+    cout << "Chuoi vua nhap: " << input << endl;
+    cout << "Do dai cua chuoi: " << input.length() << endl;
+
+    return 0;
+}
+
